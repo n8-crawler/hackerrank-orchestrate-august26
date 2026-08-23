078218e6903730d14acf909842e266a9c331c39a
Rashmi Ranjan Dash <ranjandash1403@gmail.com>
2026-08-23 05:51:53 +0530

    commit

--- code/pipeline/pipeline.py
+++ code/pipeline/pipeline.py
@@ -4,15 +4,15 @@ from services.dataloader import DataLoader
 from pipeline.nodes import build_context_node, build_prompt_node, build_response_node, image_analyse_node, parser_node, voice_analysis_node
 from pipeline.stateclass import NotificationState
 from langgraph.graph import StateGraph,START,END
-
+from langgraph.types import RetryPolicy
 graph = StateGraph(NotificationState)
-
-graph.add_node("context", build_context_node)
-graph.add_node("image", image_analyse_node)
-graph.add_node("voice", voice_analysis_node)
-graph.add_node("prompt", build_prompt_node)
-graph.add_node("reason", build_response_node)
-graph.add_node("parser", parser_node)
+retry_policy = RetryPolicy(max_attempts=2)
+graph.add_node("context", build_context_node, retry_policy=retry_policy)
+graph.add_node("image", image_analyse_node, retry_policy=retry_policy)
+graph.add_node("voice", voice_analysis_node, retry_policy=retry_policy)
+graph.add_node("prompt", build_prompt_node, retry_policy=retry_policy)
+graph.add_node("reason", build_response_node,retry_policy=retry_policy)
+graph.add_node("parser", parser_node,retry_policy=retry_policy)
 
 graph.add_edge(START, "context")
 graph.add_edge("context", "image")
